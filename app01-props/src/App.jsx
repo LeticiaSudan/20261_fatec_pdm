9eb261d47838dfc2d6262fe5d417d493fc45ac52
Leticia Sudan <lesudan01@gmail.com>
2026-03-06 18:00:32 -0300

    learn(ap04): encerra a apostila

--- app01-props/src/App.jsx
+++ app01-props/src/App.jsx
@@ -1,70 +1,71 @@
-const App = () => (
-    <div className="container border mt-2">
-        <div className="row">
-            <div className="col-12">
-                <i className="fa fa-solid fa-dog"></i>
-            </div>
-        </div>
+import Pedido from "./Pedido"
+import Cartao from "./Cartao"
+import Feedback from "./Feedback"
 
-        <div className="row">
-            <div className="col-sm-12 col-lg-6 col-xxl-3">
-                <div className="card">
-                    <div className="card-header text-muted">15/04/2025</div>
-                    <div className="card-body">
-                        <div className="d flex align-items-center">
-                           <i className="fa-solid fa-alarm-clock fa-2x"></i> 
-                        </div>
-                        <div className="ms-2 flex-grow-1 border rounded p-2">
-                            <h4 className="text-center">Despertador</h4>
-                            <p className="text-center">Despertador analógico</p>
-                        </div>
-                    </div>
+const App = () => {
+    const textoOK = "Já recebi"
+    const textoNOK = "Ainda não recebi"
+    const funcaoOK = () => alert("Que bom que você já recebeu seu pedido!")
+    const funcaoNOK = () => alert("Que pena que você ainda não recebeu seu pedido!")
+    const componenteFeedback = (
+        <Feedback 
+        textoOK={textoOK} 
+        textoNOK={textoNOK} 
+        funcaoOK={funcaoOK} 
+        funcaoNOK={funcaoNOK} />
+    )
+
+    return <div className="container border mt-2">
+
+            <div className="row">
+                <div className="col-12">
+                    <i className="fa fa-solid fa-dog"></i>
                 </div>
             </div>
-            <div className="col-sm-12 col-lg-6 col-xxl-3">
-                <div className="card">
-                    <div className="card-header text-muted">15/04/2025</div>
-                    <div className="card-body">
-                        <div className="d flex align-items-center">
-                           <i className="fa-solid fa-shirt fa-2x"></i> 
-                        </div>
-                        <div className="ms-2 flex-grow-1 border rounded p-2">
-                            <h4 className="text-center">Camiseta</h4>
-                            <p className="text-center">Camiseta preta</p>
-                        </div>
-                    </div>
+
+            <div className="row">
+                <div className="col-sm-12 col-lg-6 col-xxl-3">
+                    <Cartao cabecalho="Pedido 1">
+                        <Pedido
+                            data="15/06/2025"
+                            icone="alarm-clock"
+                            titulo="Despertador"
+                            descricao="Despertador analógico" />
+                        {componenteFeedback}
+                    </Cartao>
                 </div>
-            </div>
-            <div className="col-sm-12 col-lg-6 col-xxl-3">
-                 <div className="card">
-                    <div className="card-header text-muted">15/04/2025</div>
-                    <div className="card-body">
-                        <div className="d flex align-items-center">
-                           <i className="fa-solid fa-bowling-ball fa-2x"></i> 
-                        </div>
-                        <div className="ms-2 flex-grow-1 border rounded p-2">
-                            <h4 className="text-center">Bola de boliche</h4>
-                            <p className="text-center">Uma bola de boliche</p>
-                        </div>
-                    </div>
+                <div className="col-sm-12 col-lg-6 col-xxl-3">
+                    <Cartao cabecalho="Pedido 2">
+                        <Pedido
+                            data="17/06/2025"
+                            icone="shirt"
+                            titulo="Camisa"
+                            descricao="Uma camisa preta" />
+                        {componenteFeedback}
+                    </Cartao>
                 </div>
-            </div>
-            <div className="col-sm-12 col-lg-6 col-xxl-3">
-                 <div className="card">
-                    <div className="card-header text-muted">15/04/2025</div>
-                    <div className="card-body">
-                        <div className="d flex align-items-center">
-                           <i className="fa-solid fa-bicycle fa-2x"></i> 
-                        </div>
-                        <div className="ms-2 flex-grow-1 border rounded p-2">
-                            <h4 className="text-center">Bicicleta</h4>
-                            <p className="text-center">Uma bicicleta 18 marchas</p>
-                        </div>
-                    </div>
+                <div className="col-sm-12 col-lg-6 col-xxl-3">
+                    <Cartao cabecalho="Pedido 3">
+                        <Pedido
+                            data="14/04/2025"
+                            icone="bowling-ball"
+                            titulo="Bola de Boliche"
+                            descricao="Uma bola de boliche" />
+                        {componenteFeedback}
+                    </Cartao>
+                </div>
+                <div className="col-sm-12 col-lg-6 col-xxl-3">
+                    <Cartao cabecalho="Pedido 4">
+                        <Pedido
+                            data="17/06/2025"
+                            icone="bicycle"
+                            titulo="Bicicleta"
+                            descricao="Bicicleta 18 marchas" />
+                        {componenteFeedback}
+                    </Cartao>
                 </div>
             </div>
         </div>
-    </div>
-)
+}
 
 export default App
---
+++ app01-props/src/Pedido.jsx
@@ -0,0 +1,20 @@
+//rafce = react arrow function component export
+import React from 'react'
+//props = mecanimso do react que permite que valores sejam passados
+const Pedido = ({icone, titulo, descricao}) => {
+    return (
+        
+            <div className="d-flex">
+                <div className="d flex align-items-center">
+                    <i className={`fa-solid fa-${icone} fa-2x`}></i>
+                </div>
+                <div className="ms-2 flex-grow-1 border rounded p-2">
+                    <h4 className="text-center">{titulo}</h4>
+                    <p className="text-center">{descricao}</p>
+                </div>
+            </div>
+           
+    )
+}
+
+export default Pedido
